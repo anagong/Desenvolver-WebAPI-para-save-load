b2d421b74becf7df921b39880782d4687a9d3701
Ana Laura Da Silva Gongora <00124741@unisoacad.uniso.br>
2025-11-10 22:05:52 -0300

    4

---
+++ API_Aula_0511/Model/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Aula_0511.Model
+{
+    public class Player
+    {
+        public string Id { get; set; }
+        public int Vida { get; set; }
+        public int QuantidadeItens { get; set; }
+        public float PosicaoX { get; set; }
+        public float PosicaoY { get; set; }
+        public float PosicaoZ { get; set; }
+    }
+}
